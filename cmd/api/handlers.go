c243926b42ba6e1990fab3a4711f5ffaaefb4d82
Carlos Cabral <carlosmscabral@gmail.com>
2021-02-28 21:56:10 -0300

    fix handler

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -11,5 +11,5 @@ func (app *application) products(w http.ResponseWriter, r *http.Request) {
 		server = s
 	}
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"products": [{"name": "Boneco do Shrek", "price" : 30},{"name": "PS5", "price" : "5000"}], "server":"` + server + `"}`))
+	w.Write([]byte(`{"products": [{"name": "Boneco do Shrek", "price" : 30},{"name": "PS5", "price" : 5000}], "server":"` + server + `"}`))
 }
